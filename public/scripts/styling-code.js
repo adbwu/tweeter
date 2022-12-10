110e50592e7b1b43c7edf68a3981369490f76be3
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2022-12-10 17:37:22 -0500

    added form toggle and bounce animation to nav

---
+++ public/scripts/styling-code.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+  //jQuery for new tweet form slide animation
+  $("#call-to-action").click(function() {
+      $( ".new-tweet" ).slideToggle( "fast", "swing" );
+    });
+
+  const bounce = (() => {$("#c-t-a-chevron")
+  .animate({"padding-top": "+=3px"})
+  .animate({"padding-top": "-=3px"});
+  bounce();
+  });
+
+  bounce();
+
+  //code to "bounce" the nav chevron
+  // function bounce() {
+  //   $('i#c-t-a-chevron').animate({
+  //     top: '+=50'
+  //   }, 1000, 'bounce', function() {
+  //     $('i#c-t-a-chevron').animate({
+  //       top: '-=50'
+  //     }, 1000, 'bounce', function() {
+  //       bounce();
+  //     });
+  //   });
+  // }
+  // bounce();
+});
